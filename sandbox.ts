aaf13c3b5dad188a67cf0ede2f4d2e7ee0587f04
Anish Maharjan <anishmrzan@gmail.com>
2024-05-08 19:19:00 -0800

    tsc

--- sandbox.ts
+++
@@ -1,9 +0,0 @@
-const character = "anish";
-
-console.log(character);
-
-const inputs = document.querySelectorAll("input");
-
-inputs.forEach((input) => {
-  console.log(input);
-});
---
+++ src/sandbox.ts
@@ -0,0 +1,41 @@
+// let character = "mario";
+// // let age = 30;
+// // let isBlackBelt = false;
+
+// // // character = 20;
+// character = "anish";
+// console.log(character);
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+
+// // console.log(circ(7.5));
+
+// let names = ["anish", "astha", "snow"];
+
+// // names.push(25);
+// names.push("rayy");
+
+// // names.forEach(function (t) {
+// //   console.log(t);
+// // });
+
+// let ninja = {
+//   name: "anish",
+//   belt: "black",
+//   age: "20",
+// };
+
+// ninja.age = "40";
+// // console.log(ninja);
+
+// // ninja.skills = ["fighting", "sneaking"]; error
+
+// let characters: string;
+// let fight: string[];
+
+const logMovies = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+console.log(logMovies(1, 2));
